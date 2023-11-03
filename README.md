dc152a35ec961eebe1e5a15fb5e34f8c86921324
veiguf <veit.gufler@unibz.it>
2023-11-03 16:22:56 +0100

    first visualization

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OutdoorWeatherStation
+
+<p align=center><img width="100%" src="Visualization/Temp.svg"></p>
---
+++ Visualization/DataVisualization.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import csv
+from datetime import datetime, timedelta
+import numpy as np
+import matplotlib.pyplot as plt
+
+data_file = "/home/veit/Dokumente/OutdoorWeatherStation/SensorData.csv"
+
+with open(data_file) as csvfile:
+    reader = csv.reader(csvfile)
+    header = next(reader)
+    data = []
+    for row in reader:
+        data.append(row)
+
+array = np.array(data)
+
+TCheck = np.array(array[:, 0], dtype="datetime64")
+
+tDelta = timedelta(seconds=10)
+tMin = datetime(2023,10,1)
+tMax = datetime(2023,10,31,23,59,59,9999)
+
+keep = []
+for i in range(len(TCheck)-1):
+    if TCheck[i] >= tMin and TCheck[i] <= tMax:
+        if i == 0:
+            keep.append(i)
+        elif (TCheck[i]-TCheck[i-1]) > tDelta:
+            keep.append(i)
+
+DateTime = np.array(array[keep, 0], dtype="datetime64")
+Temperature = np.array(array[keep, 1], dtype="float")
+Humidity = np.array(array[keep, 2], dtype="int")
+WindSpeed = np.array(array[keep, 3], dtype="float")
+GustSpeed = np.array(array[keep, 4], dtype="float")
+WindDirection = np.array(array[keep, 5], dtype="str")
+Rain = np.array(array[keep, 6], dtype="float")
+BatteryStatus = np.array(array[keep, 7], dtype="str")
+
+plt.figure()
+plt.plot(DateTime, Temperature, color="C0")
+plt.ylabel(header[1])
+plt.xlim(DateTime[0], DateTime[-1])
+plt.xticks(rotation=45, ha='right')
+plt.title(tMin.strftime("%B %d, %Y")+" - "+tMax.strftime("%B %d, %Y"))
+plt.savefig("Temp.svg")
+
+plt.figure()
+plt.plot(DateTime, Humidity, color="C1")
+plt.ylabel(header[2])
+plt.xlim(DateTime[0], DateTime[-1])
+plt.ylim(0, 100)
+plt.xticks(rotation=45, ha='right')
+plt.title(tMin.strftime("%B %d, %Y")+" - "+tMax.strftime("%B %d, %Y"))
+plt.savefig("Hum.svg")
+
+plt.figure()
+plt.plot(DateTime, Rain, color="C4")
+plt.ylabel(header[6])
+plt.xlim(DateTime[0], DateTime[-1])
+plt.xticks(rotation=45, ha='right')
+plt.title(tMin.strftime("%B %d, %Y")+" - "+tMax.strftime("%B %d, %Y"))
+plt.savefig("Rain.svg")
+
+plt.figure()
+plt.hist(WindSpeed, bins=range(int(WindSpeed.max()+1)+1), density=True, color="C2")
+plt.xlabel(header[3])
+plt.ylabel("Relative Frequency")
+plt.title(tMin.strftime("%B %d, %Y")+" - "+tMax.strftime("%B %d, %Y"))
+plt.savefig("WindSpeed.svg")
+
+plt.figure()
+plt.hist(GustSpeed, bins=range(int(GustSpeed.max()+1)+1), density=True, color="C3")
+plt.xlabel(header[4])
+plt.ylabel("Relative Frequency")
+plt.title(tMin.strftime("%B %d, %Y")+" - "+tMax.strftime("%B %d, %Y"))
+plt.savefig("GustSpeed.svg")
