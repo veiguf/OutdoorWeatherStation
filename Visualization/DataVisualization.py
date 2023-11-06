b5848e816d7158fc4f24fbd1062f8fc7b6e292f7
veiguf <veit.gufler@unibz.it>
2023-11-06 17:52:53 +0100

    visualization october

--- Visualization/DataVisualization.py
+++ Visualization/DataVisualization.py
@@ -16,17 +16,19 @@ with open(data_file) as csvfile:
     for row in reader:
         data.append(row)
 
+now = datetime.now()
+now = datetime(2023,10,31,23,59,59,999999)
+
 array = np.array(data)
 
 TCheck = np.array(array[:, 0], dtype="datetime64")
 
 tDelta = timedelta(seconds=10)
-tMin = datetime(2023,10,1)
-tMax = datetime(2023,10,31,23,59,59,9999)
+tMin = datetime(now.year,now.month,1)
 
 keep = []
 for i in range(len(TCheck)-1):
-    if TCheck[i] >= tMin and TCheck[i] <= tMax:
+    if TCheck[i] >= tMin and TCheck[i] <= now:
         if i == 0:
             keep.append(i)
         elif (TCheck[i]-TCheck[i-1]) > tDelta:
@@ -46,8 +48,10 @@ plt.plot(DateTime, Temperature, color="C0")
 plt.ylabel(header[1])
 plt.xlim(DateTime[0], DateTime[-1])
 plt.xticks(rotation=45, ha='right')
-plt.title(tMin.strftime("%B %d, %Y")+" - "+tMax.strftime("%B %d, %Y"))
-plt.savefig("Temp.svg")
+plt.title(now.strftime("%B %Y"))
+plt.grid()
+plt.tight_layout()
+plt.savefig(now.strftime("%Y-%m")+"_Temp.svg")
 
 plt.figure()
 plt.plot(DateTime, Humidity, color="C1")
@@ -55,27 +59,33 @@ plt.ylabel(header[2])
 plt.xlim(DateTime[0], DateTime[-1])
 plt.ylim(0, 100)
 plt.xticks(rotation=45, ha='right')
-plt.title(tMin.strftime("%B %d, %Y")+" - "+tMax.strftime("%B %d, %Y"))
-plt.savefig("Hum.svg")
+plt.title(now.strftime("%B %Y"))
+plt.grid()
+plt.tight_layout()
+plt.savefig(now.strftime("%Y-%m")+"_Hum.svg")
 
 plt.figure()
 plt.plot(DateTime, Rain, color="C4")
 plt.ylabel(header[6])
 plt.xlim(DateTime[0], DateTime[-1])
 plt.xticks(rotation=45, ha='right')
-plt.title(tMin.strftime("%B %d, %Y")+" - "+tMax.strftime("%B %d, %Y"))
-plt.savefig("Rain.svg")
+plt.title(now.strftime("%B %Y"))
+plt.grid()
+plt.tight_layout()
+plt.savefig(now.strftime("%Y-%m")+"_Rain.svg")
 
 plt.figure()
 plt.hist(WindSpeed, bins=range(int(WindSpeed.max()+1)+1), density=True, color="C2")
 plt.xlabel(header[3])
 plt.ylabel("Relative Frequency")
-plt.title(tMin.strftime("%B %d, %Y")+" - "+tMax.strftime("%B %d, %Y"))
-plt.savefig("WindSpeed.svg")
+plt.title(now.strftime("%B %Y"))
+plt.tight_layout()
+plt.savefig(now.strftime("%Y-%m")+"_WindSpeed.svg")
 
 plt.figure()
 plt.hist(GustSpeed, bins=range(int(GustSpeed.max()+1)+1), density=True, color="C3")
 plt.xlabel(header[4])
 plt.ylabel("Relative Frequency")
-plt.title(tMin.strftime("%B %d, %Y")+" - "+tMax.strftime("%B %d, %Y"))
-plt.savefig("GustSpeed.svg")
+plt.title(now.strftime("%B %Y"))
+plt.tight_layout()
+plt.savefig(now.strftime("%Y-%m")+"_GustSpeed.svg")
