a83ff824b566a59ac47bbb9704fabadcb1456869
veiguf <veit.gufler@unibz.it>
2024-08-05 12:30:35 +0200

    cleanup

--- DataVisualization.py
+++ DataVisualization.py
@@ -19,14 +19,16 @@ with open(data_file) as csvfile:
         data.append(row)
 
 now = datetime.now()
-# now = datetime(2023,10,31,23,59,59,999999)
+# now = datetime(2024,7,31,23,59,59,999999)
+# tMin = datetime(now.year,now.month,1)
+tMin = datetime(2024,8,1)
+
+tDelta = timedelta(seconds=10)
+
 
 array = np.array(data)
 
 TCheck = np.array(array[:, 0], dtype="datetime64")
-
-tDelta = timedelta(seconds=10)
-tMin = datetime(now.year,now.month,1)
 
 keep = []
 for i in range(len(TCheck)-1):
@@ -46,7 +48,7 @@ Rain = np.array(array[keep, 6], dtype="float")
 BatteryStatus = np.array(array[keep, 7], dtype="str")
 
 fig, axs = plt.subplots(3, 1, sharex='col', figsize=(7,7))
-fig.suptitle("Weather data - "+now.strftime("%B %Y"))
+fig.suptitle("Weather data: "+tMin.strftime("%B %Y")+" - "+now.strftime("%B %Y"))
 axs[0].plot(DateTime, Temperature, ls="", marker=".", color="C0")
 axs[0].set_ylabel(header[1])
 axs[0].grid()
@@ -60,10 +62,10 @@ axs[2].grid()
 axs[2].set_xlim(DateTime[0], DateTime[-1])
 axs[2].tick_params(axis='x', labelrotation=45)
 plt.tight_layout()
-plt.savefig("/home/veit/Dokumente/OutdoorWeatherStation/Visualization/"+now.strftime("%Y-%m")+"_THR.svg")
+plt.savefig("/home/veit/Dokumente/OutdoorWeatherStation/Visualization/"+tMin.strftime("%Y-%m")+"_THR.svg")
 
 fig, axs = plt.subplots(1, 2, sharey='row', figsize=(7,3.5))
-fig.suptitle("Wind speed histograms - "+now.strftime("%B %Y"))
+fig.suptitle("Wind speed histograms: "+tMin.strftime("%B %Y")+" - "+now.strftime("%B %Y"))
 axs[0].hist(WindSpeed, bins=range(int(WindSpeed.max()+1)+1), density=True, color="C2")
 axs[0].set_xlabel(header[3])
 axs[0].set_ylabel("Relative Frequency")
@@ -72,7 +74,7 @@ axs[1].hist(GustSpeed, bins=range(int(GustSpeed.max()+1)+1), density=True, color
 axs[1].set_xlabel(header[4])
 axs[1].grid(axis='y')
 plt.tight_layout()
-plt.savefig("/home/veit/Dokumente/OutdoorWeatherStation/Visualization/"+now.strftime("%Y-%m")+"_Wind.svg")
+plt.savefig("/home/veit/Dokumente/OutdoorWeatherStation/Visualization/"+tMin.strftime("%Y-%m")+"_Wind.svg")
 
 repo = Repo('/home/veit/Dokumente/OutdoorWeatherStation')  # if repo is CWD just do '.'
 repo.index.add(['Visualization'])
